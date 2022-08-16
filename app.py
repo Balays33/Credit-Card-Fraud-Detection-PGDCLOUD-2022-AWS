15f0b242c7c18a483f8c51be13b61f20986d8944
EC2 Default User <ec2-user@ip-172-31-19-149.eu-west-1.compute.internal>
2022-08-16 20:58:58 +0000

    new file

--- app.py
+++ app.py
@@ -12,14 +12,21 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn import metrics
-from flask import Flask, request, render_template
+#from flask import Flask, request, render_template
 import pickle
+
+# ad new
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import datetime
 
 app = Flask("__name__")
 
 
-
 q = ""
+
+model = pickle.load(open('model.pkl', 'rb'))
+features = []
 
 @app.route("/")
 def loadPage():
@@ -29,29 +36,21 @@ def loadPage():
 @app.route("/", methods=['POST'])
 def predict():
     
-    inputQuery1 = request.form['query1']
-    inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = [x for x in request.form.values()]
+    print(features)
+    date_format = datetime.datetime.strptime(features[-1], "%Y-%m-%d")
+    features[-1] = datetime.datetime.timestamp(date_format)
+    print(features[-1])
+    final_features = [np.array(features[:-1])]
+    print(final_features)
+    prediction = model.predict(final_features)
 
-    model = pickle.load(open("model.sav", "rb"))
-    
-    
-    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
-    new_df = pd.DataFrame(data, columns = ['texture_mean', 'perimeter_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean'])
-    
-    single = model.predict(new_df)
-    probablity = model.predict_proba(new_df)[:,1]
-    
-    if single==1:
-        o1 = "The patient is diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
-    else:
-        o1 = "The patient is not diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
-        
-    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
+    output = prediction
+
+    return render_template('index.html', prediction_text='The probability that this transaction is fraudulent is {}'.format(output))
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
